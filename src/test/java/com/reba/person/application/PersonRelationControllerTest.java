01b57d7808fc34b99d143187a305d1d30caa7432
jesus.rios <jesus.rios@kinandcarta.com>
2024-03-10 15:14:40 -0500

    feat: test of PersonRelationService and controller was added

--- src/test/java/com/reba/person/application/PersonRelationControllerTest.java
+++ src/test/java/com/reba/person/application/PersonRelationControllerTest.java
@@ -1,4 +1,51 @@
+package com.reba.person.application;
+
+import com.reba.person.application.service.PersonRelationService;
+import com.reba.person.domain.model.PersonRelation;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.ResponseEntity;
+
+import java.util.UUID;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
 class PersonRelationControllerTest {
-  
+    @Mock
+    private PersonRelationService personRelationService;
+
+    @InjectMocks
+    private PersonRelationController personRelationController;
+
+    @BeforeEach
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void testSetParentRelation() {
+        UUID id1 = UUID.randomUUID();
+        UUID id2 = UUID.randomUUID();
+        when(personRelationService.setParentRelation(id1, id2)).thenReturn(new PersonRelation(null, id1, id2));
+
+        ResponseEntity<String> response = personRelationController.setParentRelation(id1, id2);
+        assertEquals(200, response.getStatusCodeValue());
+        assertEquals("Person with ID " + id1 + " is the parent of the person with ID " + id2, response.getBody());
+    }
+
+    @Test
+    public void testGetRelation() {
+        UUID id1 = UUID.randomUUID();
+        UUID id2 = UUID.randomUUID();
+        String relation = "parent";
+        when(personRelationService.getRelation(id1, id2)).thenReturn(relation);
+
+        ResponseEntity<String> response = personRelationController.getRelation(id1, id2);
+        assertEquals(200, response.getStatusCodeValue());
+        assertEquals(relation, response.getBody());
+    }
 }
--- src/test/java/com/reba/person/application/service/PersonRelationServiceTest.java
+++ src/test/java/com/reba/person/application/service/PersonRelationServiceTest.java
@@ -1,4 +1,56 @@
+package com.reba.person.application.service;
+
+import com.reba.person.domain.model.ContactData;
+import com.reba.person.domain.model.Person;
+import com.reba.person.domain.model.PersonRelation;
+import com.reba.person.domain.model.enums.ContactDataTypeEnum;
+import com.reba.person.domain.model.enums.CountryEnum;
+import com.reba.person.domain.model.enums.DocumentTypeEnum;
+import com.reba.person.infrastructure.repository.JpaPersonRelationRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
 class PersonRelationServiceTest {
-  
+
+    @Mock
+    private JpaPersonRelationRepository personRelationRepository;
+
+    @Mock
+    private PersonService personService;
+
+    @InjectMocks
+    private PersonRelationService personRelationService;
+
+    @BeforeEach
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void testSetParentRelation() {
+        when(personRelationRepository.save(any())).thenReturn(new PersonRelation(null, UUID.randomUUID(), UUID.randomUUID()));
+        when(personRelationRepository.existsByParentIdAndChildId(any(), any())).thenReturn(false);
+        when(personService.existsById(any())).thenReturn(true);
+        when(personService.getPersonById(any())).thenReturn(new Person(UUID.fromString("2a752c82-6019-4c14-a24d-43aa17a5a18f"), "Juan", DocumentTypeEnum.CI, "1234567890", CountryEnum.BRA, LocalDate.parse("2006-03-09"), List.of(new ContactData(ContactDataTypeEnum.PHONE, "+5491155443322"))));
+
+        PersonRelation personRelation = personRelationService.setParentRelation(UUID.randomUUID(), UUID.randomUUID());
+        assertNotNull(personRelation);
+    }
+
+    @Test
+    public void testGetRelation() {
+        String relation = personRelationService.getRelation(UUID.randomUUID(), UUID.randomUUID());
+        assertEquals("UNKNOWN", relation);
+    }
 }
